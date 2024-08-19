8168786116bf8ddf482d53f3c5ec05443e2a579a
OrdinaryLime <158983993+OrdinaryLime@users.noreply.github.com>
2024-08-19 23:04:14 +0800

    initial.py

--- physics portal/initial.py
+++ physics portal/initial.py
@@ -1,7 +1,4 @@
 # creating initital database and csv file
-import csv
-import sqlite3
-
 conn = sqlite3.connect('student.db')
 cursor = conn.cursor()
 
@@ -11,24 +8,22 @@ CREATE TABLE Student(
     name TEXT NOT NULL,
     cg TEXT NOT NULL,
     email TEXT NOT NULL,
-    password NOT NULL,
-    w_letter INTEGER,
+    password TEXT NOT NULL,
+    w_letter INTEGER
 );
 """)
 cursor.execute("""
 CREATE TABLE Submission (
     assg_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    due_date TEXT,
+    due_date TEXT
 );
 """)
 cursor.execute("""
 CREATE TABLE StudentSubmission (
     stu_id INTEGER,
-    assg_id TEXT,
+    assg_id INTGER,
     status TEXT,
-    
-
     FOREIGN KEY (stu_id) REFERENCES Student(stu_id),
     FOREIGN KEY (assg_id) REFERENCES Submission(assg_id)
 );
@@ -40,5 +35,5 @@ conn.close() # close the connection
 # file closes after with block
 with open("student.csv", 'w') as file:
     writer = csv.writer(file)
-    writer.writerow(['Student', 'cg', 'Remarks'])
-                    
+    writer.writerow(['Student', 'cg', 'Remarks'])               
+
